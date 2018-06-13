806cd75ab5ff46d9f57eed495b96f3fa03581144
Eric Warnke <ericew@gmail.com>
2018-06-13 09:27:13 -0400

    grab usb token

--- test.py
+++ test.py
@@ -7,8 +7,13 @@ from secret import edgeos_url,username,password
 
 s = edgeos_web(edgeos_url, username=username,password=password, verify=False)
 s.login()
-s.post(edgeos_url+'/api/auth.json', json={'username':username, 'password': password})
-
+usg_token = ""
+try:
+    out = s.post(edgeos_url+'/api/auth.json', json={'username':username, 'password': password})
+    usg_token = out.json()['token']
+except:
+    pass 
+    
 print("Sleeping 5 to make sure the session id {} is in the filesystem".format(s.session_id))
 sleep(5)
 

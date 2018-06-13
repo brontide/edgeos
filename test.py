a069c32adad270263755fe335bba45dc9ed50283
Eric Warnke <ericew@gmail.com>
2018-06-12 22:47:57 -0400

    usg fix

--- test.py
+++ test.py
@@ -6,11 +6,14 @@ from secret import edgeos_url,username,password
 
 s = edgeos_web(edgeos_url, username=username,password=password, verify=False)
 s.login()
+s.post(edgeos_url+'/api/auth.json', json={'username':username, 'password': password})
 
 session_id = s.session_id
 print(session_id)
 
-ws = websocket.create_connection(s.wsurl, sslopt={"cert_reqs": ssl.CERT_NONE}, origin=s._endpoint, timeout=20)
+cookie = '; '.join(["{}={}".format(*x) for x in s.cookies.items()])
+
+ws = websocket.create_connection(s.wsurl, sslopt={"cert_reqs": ssl.CERT_NONE}, origin=s._endpoint, timeout=20, cookie=cookie)
 ews = edgeos_webstream(ws,s.session_id)
 print(ews.status)
 print(ews.subscribe())

b0d3985d916aec3d515928e7b07d26c7e284ffbb
Eric Warnke <ericew@gmail.com>
2018-06-13 09:06:24 -0400

    small change

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import websocket
 import ssl
 from edgeos import edgeos_webstream, edgeos_web
+from time import sleep
 
 from secret import edgeos_url,username,password
 
@@ -8,8 +9,8 @@ s = edgeos_web(edgeos_url, username=username,password=password, verify=False)
 s.login()
 s.post(edgeos_url+'/api/auth.json', json={'username':username, 'password': password})
 
-session_id = s.session_id
-print(session_id)
+print("Sleeping 5 to make sure the session id {} is in the filesystem".format(s.session_id))
+sleep(5)
 
 ws = websocket.create_connection(s.wsurl, sslopt={"cert_reqs": ssl.CERT_NONE}, origin=s._endpoint, timeout=60, cookie=s.cookies_as_str())
 ews = edgeos_webstream(ws,s.session_id)
